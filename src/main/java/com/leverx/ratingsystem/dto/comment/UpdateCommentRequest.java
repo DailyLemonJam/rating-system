1a87a1f79b7164848a4b0aabc96902f65268e42f
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-03-14 04:47:16 +0300

    Added validation to UpdateCommentRequest
    And of course added necessary validation dependency

--- src/main/java/com/leverx/ratingsystem/dto/comment/UpdateCommentRequest.java
+++ src/main/java/com/leverx/ratingsystem/dto/comment/UpdateCommentRequest.java
@@ -1,4 +1,9 @@
 package com.leverx.ratingsystem.dto.comment;
 
-public record UpdateCommentRequest(String newMessage, int newGrade) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateCommentRequest(@NotNull String newMessage,
+                                   @Min(1) @Max(5) int newGrade) {
 }

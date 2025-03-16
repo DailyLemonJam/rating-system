3ab2045e0163b1fd1f25d316d94ba3bdc08e0449
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-03-16 16:20:26 +0300

    Added getAdminRole in RoleService
    Also findByName method in RoleRepository is not Optional anymore

--- src/main/java/com/leverx/ratingsystem/service/RoleService.java
+++ src/main/java/com/leverx/ratingsystem/service/RoleService.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RoleService {
-
     private final RoleRepository roleRepository;
 
     public Role getUserRole() {
-        return roleRepository.findByName("ROLE_USER").get();
+        return roleRepository.findByName("ROLE_USER");
+    }
+
+    public Role getAdminRole() {
+        return roleRepository.findByName("ROLE_ADMIN");
     }
 
 }

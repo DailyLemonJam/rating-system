248459fc1ed9a44c80f327df7b572084696896d1
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2025-03-17 03:25:17 +0300

    Setup RedisConfig

--- src/main/java/com/leverx/ratingsystem/config/RedisConfiguration.java
+++ src/main/java/com/leverx/ratingsystem/config/RedisConfiguration.java
@@ -1,8 +1,24 @@
 package com.leverx.ratingsystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
-    // TODO: Redis config
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        var template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
 }
